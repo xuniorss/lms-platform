300bc1fe2e78aefccb4c100315156014447bd96a
xuniorss <fortunatojunior2020@gmail.com>
2023-09-28 14:59:53 -0300

    feat: Courses Data Table

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,22 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import { prismadb } from '@/lib/prismadb'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { columns } from './_components/columns'
+import { DataTable } from './_components/data-table'
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+	const { userId } = auth()
+
+	if (!userId) return redirect('/')
+
+	const courses = await prismadb.course.findMany({
+		where: { userId },
+		orderBy: { createdAt: 'desc' },
+	})
+
 	return (
 		<section className="p-6">
-			<Link href="/teacher/create">
-				<Button>Novo curso</Button>
-			</Link>
+			<DataTable columns={columns} data={courses} />
 		</section>
 	)
 }
