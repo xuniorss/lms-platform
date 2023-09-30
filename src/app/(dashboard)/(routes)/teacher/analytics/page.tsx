225e74f3b8491c59d466941495e2d66ccd86039f
xuniorss <fortunatojunior2020@gmail.com>
2023-09-30 11:30:01 -0300

    feat: Teacher Analytics Dashboard

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,3 +1,28 @@
-export default function AnalyticsPage() {
-	return <div></div>
+import { getAnalytics } from '@/actions/get-analytics'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+import { Chart } from './_components/chart'
+import { DataCard } from './_components/data-card'
+
+export default async function AnalyticsPage() {
+	const { userId } = auth()
+
+	if (!userId) return redirect('/')
+
+	const { data, totalRevenue, totalSales } = await getAnalytics(userId)
+
+	return (
+		<section className="p-6">
+			<div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+				<DataCard
+					label="Rendimento total"
+					value={totalRevenue}
+					shouldFormat
+				/>
+				<DataCard label="Vendas totais" value={totalSales} />
+			</div>
+			<Chart data={data} />
+		</section>
+	)
 }
